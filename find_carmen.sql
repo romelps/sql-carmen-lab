8cf4d450a46ba20ad0fab9d248edd78f4b4b17aa
Romel <romelps98@gmail.com>
2024-06-03 23:57:57 -0600

    Commit: CARMEN SANDIEGO-I found Carmen Sandiego

--- find_carmen.sql
+++ find_carmen.sql
@@ -55,11 +55,22 @@ Answer:
 Serravalle
 
 -- Clue #5: Oh no, she pulled a switch – there are two cities with very similar names, but in totally different parts of the globe! She's headed to South America as we speak; go find a city whose name is like the one we were headed to, but doesn't end the same. Find out the city, and do another search for what country it's in. Hurry!
+Code:
+SELECT name, countrycode FROM city WHERE name LIKE 'Serra%';
+SELECT name FROM country WHERE code = 'BRA';
 
+Answer:
+Serra, BRA
+Brazil
 
 -- Clue #6: We're close! Our South American agent says she just got a taxi at the airport, and is headed towards the capital! Look up the country's capital, and get there pronto! Send us the name of where you're headed and we'll follow right behind you!
+Code:
+SELECT capital FROM country WHERE code = 'BRA';
+SELECT name FROM city WHERE countrycode = 'BRA' AND id=211;
 
-
+Answer: 
+capital: 211
+name: Brasï¿½lia
 
 -- Clue #7: She knows we're on to her – her taxi dropped her off at the international airport, and she beat us to the boarding gates. We have one chance to catch her, we just have to know where she's heading and beat her to the landing dock.
 
@@ -74,5 +85,8 @@ Serravalle
 
 
 -- We're counting on you, gumshoe. Find out where she's headed, send us the info, and we'll be sure to meet her at the gates with bells on.
+Code:
+ SELECT name FROM city WHERE population = 91084;
 
-
+ Answer:
+ Santa Monica
